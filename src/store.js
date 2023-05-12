0cbdfa1db3c656c29adf24a4a9e94a25d7548bc5
bboysquiz <mrkiraller@gmail.com>
2023-05-12 12:58:28 +0300

    3 tasks are done

--- src/store.js
+++ src/store.js
@@ -44,8 +44,9 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, {code: Date.now(), title: 'Новая запись', selectedCounter: 0, selected: false}]
     })
+    console.log(this.getState())
   };
 
   /**
@@ -68,11 +69,22 @@ class Store {
       ...this.state,
       list: this.state.list.map(item => {
         if (item.code === code) {
-          item.selected = !item.selected;
+          if (item.selected === false){
+            item.selected = true;
+            item.selectedCounter++;
+          } else {
+            item.selected = false;
+          }
+        }
+        if (item.code !== code) {
+          if (item.selected === true) {
+            item.selected = false;
+          }
         }
         return item;
       })
     })
+    console.log(this.getState())
   }
 }
 
